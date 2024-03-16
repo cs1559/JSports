905cee15b5997d13e0e1198d6833b246b6bdb2c0
Chris Strieter <cs1559@sbcglobal.net>
2024-03-16 08:06:10 -0500

    added the onchange function from jsports.js and new document
    onload function to load the standings upon page load.

--- JSports/media/js/standings.js
+++ JSports/media/js/standings.js
@@ -1,10 +1,9 @@
-
+// Used to auto load the standings page via AJAX passing no programid.
 
 jQuery( document ).ready(function() {
 
 		jQuery.ajax({
 			    url: 'index.php?option=com_jsports&view=standings&layout=show&tmpl=component',
-			    data: '&programid=' + jQuery("#program-list").first().val(),
 			    type: 'POST',
 				dataType: 'html',
 				beforeSend: function() {
@@ -22,6 +21,28 @@ jQuery( document ).ready(function() {
 
 });
 
+// Function to retrieve standings based on change in the program list.
+jQuery("#program-list").change(function(){
+
+		jQuery.ajax({
+			    url: 'index.php?option=com_jsports&view=standings&layout=show&tmpl=component',
+			    data: '&programid=' + jQuery("#program-list option:selected").val(),
+			    type: 'POST',
+				dataType: 'html',
+				beforeSend: function() {
+					spinner.removeAttribute('hidden');
+				},	
+	    		success: function(data){
+	    			jQuery("#standings-container").html(data);
+	    			jQuery("#standings-container").focus();
+	    		},
+	    		complete: function() {
+	  				jQuery("#standings-activity-image").html("");   
+	  				spinner.setAttribute('hidden','');
+	    		}	    		
+			});		
+	
+});
 
 
 	
