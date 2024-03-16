f364d00910ee0cda2102a279d5f53c716d0b9411
Chris Strieter <cs1559@sbcglobal.net>
2024-03-16 08:05:15 -0500

    moved the onChange jquery function to the standings.js file.

--- JSports/media/js/jsports.js
+++ JSports/media/js/jsports.js
@@ -6,25 +6,3 @@ jQuery("#profile-actions").change(function(){
  	window.location = jQuery(this).val();
 });
 
-jQuery("#program-list").change(function(){
-
-		jQuery.ajax({
-			    url: 'index.php?option=com_jsports&view=standings&layout=show&tmpl=component',
-			    data: '&programid=' + jQuery("#program-list option:selected").val(),
-			    type: 'POST',
-				dataType: 'html',
-				beforeSend: function() {
-					spinner.removeAttribute('hidden');
-				},	
-	    		success: function(data){
-	    			jQuery("#standings-container").html(data);
-	    			jQuery("#standings-container").focus();
-	    		},
-	    		complete: function() {
-	  				jQuery("#standings-activity-image").html("");   
-	  				spinner.setAttribute('hidden','');
-	    		}	    		
-			});		
-	
-});
-
