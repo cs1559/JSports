047aea600bf3dda2b4b285ac4c819a98d74638a8
Chris Strieter <cs1559@sbcglobal.net>
2024-03-27 17:03:46 -0500

    Added REFRESH STANDINGS function

--- JSports/admin/src/Controller/ProgramsController.php
+++ JSports/admin/src/Controller/ProgramsController.php
@@ -16,6 +16,8 @@ defined('_JEXEC') or die;
 
 use Joomla\CMS\MVC\Controller\AdminController;
 use Joomla\CMS\Factory;
+use FP4P\Component\JSports\Site\Objects\Standings\StandingsEngine;
+use FP4P\Component\JSports\Site\Services\ProgramsService;
 
 class ProgramsController extends AdminController
 {
@@ -47,5 +49,40 @@ class ProgramsController extends AdminController
         } else {
             $this->setRedirect('index.php?option=com_jsports&view=closeprogram&id=' . $ids[0]);
         }
+    }
+    
+    public function refreshstandings(){
+        $ids = $this->input->post->get('cid', array(), 'array');
+        if (count($ids) > 1) {
+            Factory::getApplication()->enqueueMessage("You can only REFRESH STANDINGS one program at a time", 'warning');
+            $this->setRedirect('index.php?option=com_jsports&view=programs');
+            return;
+        } 
+        
+        $program = ProgramsService::getItem($ids[0]);
+
+        if ($program->registrationonly) {
+            Factory::getApplication()->enqueueMessage("REFRESH STANDINGS does not apply to REGISTRATION ONLY programs", 'warning');
+            $this->setRedirect('index.php?option=com_jsports&view=programs');
+            return;
+        }
+        
+        if ($program->status != 'A') {
+            Factory::getApplication()->enqueueMessage("You can only REFRESH STANDINGS on an ACTIVE program", 'warning');
+            $this->setRedirect('index.php?option=com_jsports&view=programs');
+            return;
+        }
+
+
+        $engine = new StandingsEngine();
+        
+        if ($engine->generateStandings($program->id)) {
+            Factory::getApplication()->enqueueMessage("Standings Refreshed", 'message');
+            $this->setRedirect('index.php?option=com_jsports&view=programs');
+        } else {
+            Factory::getApplication()->enqueueMessage("There was an error when refreshing the standings", 'warning');
+            $this->setRedirect('index.php?option=com_jsports&view=programs');
+        }
+        
     }
 }
