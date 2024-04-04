430dfab218f9d4d7f69952f1462033673c1e3b8c
Chris Strieter <cs1559@sbcglobal.net>
2024-04-04 16:34:03 -0500

    miscellaneous changes.  still work in progress

--- JSports/admin/tmpl/dashboard/default.php
+++ JSports/admin/tmpl/dashboard/default.php
@@ -1,6 +1,72 @@
+<?php 
 
-<h2><?php echo $this->title; ?></h2>
-<h3><?php echo $this->version; ?></h3>
+// Load Web Asset Manager
+$wa = $this->document->getWebAssetManager();
+$wa->getRegistry()->addExtensionRegistryFile('com_jsports');
+$wa->useStyle('com_jsports.admin.style');
+
+?>
+
+<div id="dashboard-container">
+  <div class="row" >
+	<div class="brand mb-3 p-3">
+			<h2><?php echo $this->title . " - " . $this->version; ?></h2>	
+	</div>
+  </DIV>
+  <div class="row">
+  		<div class="col">
+  			<div class="card border h-100 py-2 " style="min-height: 100px;">
+  				<a href="index.php?option=com_jsports&view=leagues">Organization</a>
+  			</div>
+  		</div>	
+  		<div class="col">
+  			<div class="card border h-100 py-2 " style="min-height: 100px;">
+  				<a href="index.php?option=com_jsports&view=programs">Programs</a><br/>
+  			</div>
+  		</div>
+  		<div class="col">
+  			<div class="card border h-100 py-2 " style="min-height: 100px;">
+				<a href="index.php?option=com_jsports&view=divisions">Divisions</a><br/>
+			</div>
+  		</div>
+  		<div class="col">
+  			<div class="card border h-100 py-2 " style="min-height: 100px;">
+  				<a href="index.php?option=com_jsports&view=teams">Teams</a><br/>
+  			</div>
+  		</div>
+  		<div class="col">
+  			<div class="card border h-100 py-2 " style="min-height: 100px;">
+  				<a href="index.php?option=com_jsports&view=venues">Venues</a><br/>
+  			</div>
+  		</div>
+  		<div class="col">
+  			<div class="card border h-100 py-2 " style="min-height: 100px;">
+  				<a href="index.php?option=com_jsports&view=Registrations">Registrations</a><br/>
+  			</div>
+  		</div>
+  		<div class="col">
+  			<div class="card border h-100 py-2 " style="min-height: 100px;">
+  				<a href="index.php?option=com_jsports&view=Programsetup">Program Setup</a><br/>
+  			</div>
+  		</div>
+  		<div class="col">
+			<div class="card border h-100 py-2 " style="min-height: 100px;">  		
+  				<a href="index.php?option=com_jsports&view=Rosters">Rosters</a><br/>
+  			</div>
+  		</div>
+  		<div class="col">
+  			<div class="card border h-100 py-2 " style="min-height: 100px;">
+  			<a href="index.php?option=com_jsports&view=Games">Games</a><br/>
+  			</div>
+  		</div>
+  		
+  </div>
+
+
+
+
+</div>
+
 
 <a href="index.php?option=com_jsports&view=leagues">Leagues/Organization</a><br/>
 <a href="index.php?option=com_jsports&view=programs">Programs</a><br/>
@@ -11,4 +77,6 @@
 <a href="index.php?option=com_jsports&view=Programsetup">Program Setup</a><br/>
 <a href="index.php?option=com_jsports&view=Rosters">Rosters</a><br/>
 <a href="index.php?option=com_jsports&view=Games">Games</a><br/>
-<a href="index.php?option=com_jsports&task=refreshStandings">Refresh Standings</a><br/>
+
+
+</div>
