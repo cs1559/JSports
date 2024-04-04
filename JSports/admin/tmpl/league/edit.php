2af38716f8d733100cbb4331004caa15fe80ed35
Chris Strieter <cs1559@sbcglobal.net>
2024-04-04 15:20:40 -0500

    removed registration and roster options to program level attr.

--- JSports/admin/tmpl/league/edit.php
+++ JSports/admin/tmpl/league/edit.php
@@ -27,39 +27,24 @@ HTMLHelper::_('behavior.keepalive');
 	<div>
 		<?php echo HTMLHelper::_('uitab.startTabSet', 'myTab', array('active' => 'details')); ?>
 
-		<?php echo HTMLHelper::_('uitab.addTab', 'myTab', 'details', Text::_('COM_JSPORTS_DETAILS')); ?>
-		<div class="row">
-			<div class="col-md-9">
-				<div class="row">
-					<div class="col-md-8">
-						<?php echo $this->form->renderField('description'); ?>
-					</div>
-				</div>
-			</div>
-			<div class="col-md-3">
-				<div class="card card-light">
-					<div class="card-body">
-						<?php echo LayoutHelper::render('joomla.edit.global', $this); ?>
-					</div>
-				</div>
-			</div>
-		</div>
-		<?php echo HTMLHelper::_('uitab.endTab'); ?>
-
-		<?php echo HTMLHelper::_('uitab.addTab', 'myTab', 'Options', Text::_('COM_JSPORTS_OPTIONS')); ?>
-		<div class="row">
-			<div class="col-md-9">
-				<div class="row">
-					<div class="col-md-8">
-						<?php echo $this->form->renderField('registrationenabled'); ?>
-						<?php echo $this->form->renderField('rostersenabled'); ?>
-						<?php echo $this->form->renderField('rosterslocked'); ?>
-					</div>
-				</div>
-			</div>
-		</div>
-		<?php echo HTMLHelper::_('uitab.endTab'); ?>
-
+    		<?php echo HTMLHelper::_('uitab.addTab', 'myTab', 'details', Text::_('COM_JSPORTS_DETAILS')); ?>
+    		<div class="row">
+    			<div class="col-md-9">
+    				<div class="row">
+    					<div class="col-md-8">
+    						<?php echo $this->form->renderField('description'); ?>
+    					</div>
+    				</div>
+    			</div>
+    			<div class="col-md-3">
+    				<div class="card card-light">
+    					<div class="card-body">
+    						<?php echo LayoutHelper::render('joomla.edit.global', $this); ?>
+    					</div>
+    				</div>
+    			</div>
+    		</div>
+    		<?php echo HTMLHelper::_('uitab.endTab'); ?>
 
 		<?php echo HTMLHelper::_('uitab.endTabSet'); ?>
 	</div>
