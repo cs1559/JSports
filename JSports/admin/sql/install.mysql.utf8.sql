9da551e8e41ed4507bfafdd67959f513a2f3faf5
Chris Strieter <cs1559@sbcglobal.net>
2024-04-04 16:33:15 -0500

    added sql to create new tables

--- JSports/admin/sql/install.mysql.utf8.sql
+++ JSports/admin/sql/install.mysql.utf8.sql
@@ -1,12 +1,24 @@
-CREATE TABLE IF NOT EXISTS `#__jsports_leagues` (
+CREATE TABLE IF NOT EXISTS `#__jsports_groups` (
+    `code` VARCHAR(4) NOT NULL,
+    `name` VARCHAR(30) NOT NULL,
+    `published` TINYINT(3) DEFAULT 0,
+    PRIMARY KEY (`code`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS `#__jsports_groups_items` (
     `id` INT(11) NOT NULL AUTO_INCREMENT,
-    `name` VARCHAR(100),
-    `abbr` VARCHAR(10),
-    `description` MEDIUMTEXT,
-    `published` TINYINT(3),
-    `configuration` MEDIUMTEXT,
+    `groupcode` VARCHAR(4) NOT NULL,
+    `code` VARCHAR(4) NOT NULL,
+    `name` VARCHAR(30) NOT NULL,
     PRIMARY KEY (`id`)
-)
-    ENGINE=InnoDB
-    DEFAULT CHARSET=utf8mb4
-    DEFAULT COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS `#__jsports_action_logs` (
+    `id` INT(11) NOT NULL AUTO_INCREMENT,
+    `logdate` datetime NOT NULL,
+    `userid` VARCHAR(30) NOT NULL,
+    `username` VARCHAR(30) NOT NULL,
+    `msg` VARCHAR(255) NOT NULL,
+    PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
