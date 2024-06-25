01aabcf1fc850642c4be08e029cf23f76b8f6328
Chris Strieter <cs1559@sbcglobal.net>
2024-06-25 14:21:46 -0500

    made changes

--- JSports/media/js/rosters.js
+++ JSports/media/js/rosters.js
@@ -4,10 +4,9 @@ jQuery( document ).ready(function() {
     console.log( "ready!" );
     val = jQuery('#filter_teamid').val();
     if (val === 0) {
-		alert('hide element');
 		jQuery('.button-new').prop('disabled',true);
 	} else {
-		alert('show button');
+		jQuery('.button-new').prop('disabled',false);
 	}
 
 });
