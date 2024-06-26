16e63d4c25a36dcc5329fe85f266f05b6182bb82
Chris Strieter <cs1559@sbcglobal.net>
2024-06-26 07:24:19 -0500

    general modifications to dashboard.

--- JSports/admin/tmpl/dashboard/default.php
+++ JSports/admin/tmpl/dashboard/default.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 
 // Load Web Asset Manager
 $wa = $this->document->getWebAssetManager();
@@ -7,76 +7,159 @@ $wa->useStyle('com_jsports.admin.style');
 
 ?>
 
-<div id="dashboard-container">
-  <div class="row" >
-	<div class="brand mb-3 p-3">
-			<h2><?php echo $this->title . " - " . $this->version; ?></h2>	
-	</div>
-  </DIV>
-  <div class="row">
-  		<div class="col">
-  			<div class="card border h-100 py-2 " style="min-height: 100px;">
-  				<a href="index.php?option=com_jsports&view=leagues">Organization</a>
-  			</div>
-  		</div>	
-  		<div class="col">
-  			<div class="card border h-100 py-2 " style="min-height: 100px;">
-  				<a href="index.php?option=com_jsports&view=programs">Programs</a><br/>
-  			</div>
-  		</div>
-  		<div class="col">
-  			<div class="card border h-100 py-2 " style="min-height: 100px;">
-				<a href="index.php?option=com_jsports&view=divisions">Divisions</a><br/>
+<div id="dashboard-container container">
+	<div class="row">
+		<div class="dashboard-heading brand mb-3 p-3">
+			<h2><?php echo $this->title . " - " . $this->version; ?></h2>
+		</div>
+	</DIV>
+	
+	<div class="row g-3">
+		<div class="col-12 col-lg-8">
+			<div class="dashboard-cards row row-cols-1 row-cols-md-2 row-cols-lg-3 row-cols-xxl-4 g-3 mb-3 text-center">
+				<div class="col">
+					<a href="index.php?option=com_jsports&view=leagues">
+    				<div class="card h-100 p-2 m-1">
+    					<div style="text-align: center;">
+    						<p class="card-text" ><h2>Leagues</h2>
+    							<i class="fas fa-university fa-3x"></i>
+    						</p>
+    						
+    					</div>
+    				</div>
+    				</a>
+    			</div>
+			
+				<div class="col">
+					<a href="index.php?option=com_jsports&view=programs">
+    				<div class="card h-100 p-2 m-1">
+    					<div style="text-align: center;">
+    						<p class="card-text" ><h2>Programs</h2>
+    							<i class="fas fa-trophy fa-3x"></i>
+    						</p>
+    						
+    					</div>
+    				</div>
+    				</a>
+    			</div>
+    			<div class="col">
+    				<a href="index.php?option=com_jsports&view=divisions">
+    				<div class="card h-100 p-2 m-1">
+    					<div style="text-align: center;">
+    						<p class="card-text" ><h2>Divisions</h2>
+    							<i class="fas fa-baseball-ball fa-3x"></i>
+    						</p>
+    						
+    					</div>
+    				</div>
+    				</a>
+				</div>
+    			<div class="col">
+    				<a href="index.php?option=com_jsports&view=teams">
+    				<div class="card h-100 p-2 m-1">
+    					<div style="text-align: center;">
+    						<p class="card-text" ><h2>Teams</h2>
+    							<i class="fas fa-users fa-3x"></i>
+    						</p>
+    						
+    					</div>
+    				</div>
+    				</a>
+				</div>				
+    			<div class="col">
+    				<a href="index.php?option=com_jsports&view=venues">
+    				<div class="card h-100 p-2 m-1">
+    					<div style="text-align: center;">
+    						<p class="card-text" ><h2>Venues</h2>
+    							<i class="fas fa-map-marker-alt fa-3x"></i>
+    						</p>
+    					</div>
+    				</div>
+    				</a>
+				</div>				
+
+    			<div class="col">
+    				<a href="index.php?option=com_jsports&view=registrations">
+    				<div class="card h-100 p-2 m-1">
+    					<div style="text-align: center;">
+    						<p class="card-text" ><h2>Registrations</h2>
+    							<i class="fas fa-baseball-ball fa-3x"></i>
+    						</p>
+    					</div>
+    				</div>
+    				</a>
+				</div>
+				
+    			<div class="col">
+    				<a href="index.php?option=com_jsports&view=rosters">
+    				<div class="card h-100 p-2 m-1">
+    					<div style="text-align: center;">
+    						<p class="card-text" ><h2>Rosters</h2>
+    							<i class="fas fa-paste fa-3x"></i>
+    						</p>
+    					</div>
+    				</div>
+    				</a>
+				</div>
+				
+    			<div class="col">
+    				<a href="index.php?option=com_jsports&view=games">
+    				<div class="card h-100 p-2 m-1">
+    					<div style="text-align: center;">
+    						<p class="card-text" ><h2>Games/Schedules</h2>
+    							<i class="fas fa-baseball-ball fa-3x"></i>
+    						</p>
+    						
+    					</div>
+    				</div>
+    				</a>
+				</div>
+
+    			<div class="col">
+    				<a href="index.php?option=com_jsports&view=logs">
+    				<div class="card h-100 p-2 m-1">
+    					<div style="text-align: center;">
+    						<p class="card-text" ><h2>View Logs</h2>
+    							<i class="fas fa-newspaper fa-3x"></i>
+    						</p>
+    						
+    					</div>
+    				</div>
+    				</a>
+				</div>
 			</div>
-  		</div>
-  		<div class="col">
-  			<div class="card border h-100 py-2 " style="min-height: 100px;">
-  				<a href="index.php?option=com_jsports&view=teams">Teams</a><br/>
-  			</div>
-  		</div>
-  		<div class="col">
-  			<div class="card border h-100 py-2 " style="min-height: 100px;">
-  				<a href="index.php?option=com_jsports&view=venues">Venues</a><br/>
-  			</div>
-  		</div>
-  		<div class="col">
-  			<div class="card border h-100 py-2 " style="min-height: 100px;">
-  				<a href="index.php?option=com_jsports&view=Registrations">Registrations</a><br/>
-  			</div>
-  		</div>
-  		<div class="col">
-  			<div class="card border h-100 py-2 " style="min-height: 100px;">
-  				<a href="index.php?option=com_jsports&view=Programsetup">Program Setup</a><br/>
-  			</div>
-  		</div>
-  		<div class="col">
-			<div class="card border h-100 py-2 " style="min-height: 100px;">  		
-  				<a href="index.php?option=com_jsports&view=Rosters">Rosters</a><br/>
-  			</div>
-  		</div>
-  		<div class="col">
-  			<div class="card border h-100 py-2 " style="min-height: 100px;">
-  			<a href="index.php?option=com_jsports&view=Games">Games</a><br/>
-  			</div>
-  		</div>
-  		
-  </div>
+		</div>
+		<div class="col-12 col-lg-4">
+			<h2>About JSports</h2>
+		</div>
+	</div>
+	
+</div>	
+	
+	
 
 
+<br/><br/><br/><br/>
 
 
-</div>
-
-
-<a href="index.php?option=com_jsports&view=leagues">Leagues/Organization</a><br/>
-<a href="index.php?option=com_jsports&view=programs">Programs</a><br/>
-<a href="index.php?option=com_jsports&view=divisions">Divisions</a><br/>
-<a href="index.php?option=com_jsports&view=teams">Teams</a><br/>
-<a href="index.php?option=com_jsports&view=venues">Venues</a><br/>
-<a href="index.php?option=com_jsports&view=Registrations">Registrations</a><br/>
-<a href="index.php?option=com_jsports&view=Programsetup">Program Setup</a><br/>
-<a href="index.php?option=com_jsports&view=Rosters">Rosters</a><br/>
-<a href="index.php?option=com_jsports&view=Games">Games</a><br/>
+<a href="index.php?option=com_jsports&view=leagues">Leagues/Organization</a>
+<br />
+<a href="index.php?option=com_jsports&view=programs">Programs</a>
+<br />
+<a href="index.php?option=com_jsports&view=divisions">Divisions</a>
+<br />
+<a href="index.php?option=com_jsports&view=teams">Teams</a>
+<br />
+<a href="index.php?option=com_jsports&view=venues">Venues</a>
+<br />
+<a href="index.php?option=com_jsports&view=Registrations">Registrations</a>
+<br />
+<a href="index.php?option=com_jsports&view=Programsetup">Program Setup</a>
+<br />
+<a href="index.php?option=com_jsports&view=Rosters">Rosters</a>
+<br />
+<a href="index.php?option=com_jsports&view=Games">Games</a>
+<br />
 
 
 </div>
