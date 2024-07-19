a9eceba4cb076b8296202d845543bbb22a8e3d70
Chris Strieter <cs1559@sbcglobal.net>
2024-07-19 01:55:35 -0500

    updated to use groups in the filtering vs. age group

--- JSports/admin/src/Model/RegistrationsModel.php
+++ JSports/admin/src/Model/RegistrationsModel.php
@@ -10,6 +10,12 @@
  *
  */
 
+/**
+ * CHANGE HISTORY
+ * 7/10/2024 - Changed AGE GROUP to GROUPING; commented out Delete function.
+ * 
+ */
+
 namespace FP4P\Component\JSports\Administrator\Model;
 
 defined('_JEXEC') or die;
@@ -44,7 +50,7 @@ class RegistrationsModel extends ListModel
 				'teamname', 'a.teamname',
 			    'programid', 'a.programid',
 			    'published', 'a.published',
-			    'agegroup', 'a.agegroup',
+			    'grouping', 'a.grouping',
 			);
 		}
 
@@ -69,11 +75,11 @@ class RegistrationsModel extends ListModel
 	    $app = Factory::getApplication();
 	    
 	    $programid = $app->input->get('programid', 0, 'int');
+	    $programid = $app->input->get('id', 0, 'int');
 	    if (empty($programid)) {
 	       $programid = $app->getUserState('com_jsports.programid');
 	    }
 	    $this->setState('programid', $programid);
-	    // keep the walk_id for adding new visits
 	    $app->setUserState('com_jsports.programid', $programid);
 	    
 		$search = $this->getUserStateFromRequest($this->context . '.filter.search', 'filter_search');
@@ -82,8 +88,8 @@ class RegistrationsModel extends ListModel
 		$published = $this->getUserStateFromRequest($this->context . '.filter.published', 'filter_published', '');
 		$this->setState('filter.published', $published);
 
-		$agegroup = $this->getUserStateFromRequest($this->context . '.filter.agegroup', 'filter_agegroup', '');
-		$this->setState('filter.agegroup', $agegroup);
+		$grouping = $this->getUserStateFromRequest($this->context . '.filter.grouping', 'filter_grouping', '');
+		$this->setState('filter.grouping', $grouping);
 		
 		// List state information.
 		parent::populateState($ordering, $direction);
@@ -107,8 +113,9 @@ class RegistrationsModel extends ListModel
 		// Compile the store id.
 		$id .= ':' . $this->getState('filter.search');
 		$id .= ':' . $this->getState('filter.published');
-		$id .= ':' . $this->getState('filter.programid');
-		$id .= ':' . $this->getState('filter.agegroup');
+// 		$id .= ':' . $this->getState('filter.programid');
+		$id .= ':' . $this->getState('filter.id');
+		$id .= ':' . $this->getState('filter.grouping');
 
 		return parent::getStoreId($id);
 	}
@@ -150,18 +157,19 @@ class RegistrationsModel extends ListModel
 	    }
 	    
 	    // Filter by PROGRAM.
-	    $programid = $this->getState('filter.programid');
+// 	    $programid = $this->getState('filter.programid');
+	    $programid = $this->getState('filter.id');
 	    if (!empty($programid)) {	        
 	        $query->where($db->quoteName('a.programid') . ' = :programid');
 	        $query->bind(':programid', $programid, ParameterType::INTEGER);
 	    }
 
 	    // Filter by AGE GROUP
-	    $agegroup = (string) $this->getState('filter.agegroup');
-	    if (is_numeric($agegroup))
-	    {
-	        $query->where($db->quoteName('a.agegroup') . ' = :agegroup');
-	        $query->bind(':agegroup', $agegroup, ParameterType::INTEGER);
+	    $grouping = (string) $this->getState('filter.grouping');
+	    
+	    if (strlen($grouping)>0) {
+	       $query->where($db->quoteName('a.grouping') . ' = :grouping');
+	       $query->bind(':grouping', $grouping, ParameterType::STRING);
 	    }
 	    
 	    // Filter by search in TEAMNAME.
@@ -203,9 +211,10 @@ class RegistrationsModel extends ListModel
 	}
 	
 	
+	
 
 // 	    /**
-// 	     * Method to change the published state of one or more records.
+// 	     * Method to DELETE one or more records from the database
 // 	     *
 // 	     * @param   array    &$pks   A list of the primary keys to change.
 // 	     * @param   integer  $value  The value of the published state.
@@ -214,166 +223,24 @@ class RegistrationsModel extends ListModel
 // 	     *
 // 	     * @since   4.0.0
 // 	     */
-// 	    public function publish(&$pks, $value = 1) {
+// 	    public function delete(&$pks, $value = 1) {
 	        
-// 	        $return = true;
-// 	        foreach ($pks as $id) {
-// 	            $rc = $this->processRegistration($id);
-// 	            if (!$rc) {
-// 	                $return = false;
-// 	            }
-// 	        }
-
-// 	        return $return;
-//      }
 	        
-	    
-// 	    /**
-// 	     * This function performs the business logic to process/publish a given registration.  This process will do the following:
-// 	     * 
-// 	     * 1.  Create a new team record if appropriate
-// 	     * 2.  Add the record to the Team/Program/Division mapping file.
-// 	     * 3.  Publish the registration record.  Once its published, it cannot be published again.
-// 	     * 
-// 	     * @param unknown $regid
-// 	     * @return boolean
-// 	     */
-// 	    protected function processRegistration($regid) {
-
+	        
+// 	        /* this is a very simple method to change the state of each item selected */
 // 	        $db = $this->getDatabase();
 	        
-// 	        $rsvc = new RegistrationService();
-// 	        $item = $rsvc->getItem($regid);
-
-// 	        if ($item->published) {
-// 	            Factory::getApplication()->enqueueMessage("State of a published registration cannot be changed", 'error');
-// 	            return false;
-// 	        }
-// 	        try {
-            
-
-//                 // begin transactio
-//                 $db->transactionStart();
-    
-//                 // =========================================================================================
-//                 // Create Team record
-//                 // =========================================================================================
-//                 $query = $db->getQuery(true);
-//                 $columns = array(
-//                     'id',
-//                     'name',
-//                     'alias',
-//                     'city',
-//                     'state',
-//                     'contactname',
-//                     'contactemail',
-//                     'contactphone',
-//                     'published'
-//                 );
-//                 $teamname = OutputFilter::stringURLUnicodeSlug($item->teamname);
-    
-//                 $values = array(
-//                     0,
-//                     $db->quote($item->teamname), // teamname
-//                     $db->quote($teamname), // alias
-//                     $db->quote($item->city), // city
-//                     $db->quote($item->state), // state
-//                     $db->quote($item->name), // contact name
-//                     $db->quote($item->email), // contact email
-//                     $db->quote($item->phone), // contact phone
-//                     1 // published
-//                 );
-    
-//                 $query->insert($db->quoteName('#__jsports_teams'))
-//                     ->columns($db->quoteName($columns))
-//                     ->values(implode(',', $values));
-    
-//                 $db->setQuery($query);
-//                 $db->execute();
-    	           
-//     	           // Get the row that was just inserted
-//     	        $new_row_id = $db->insertid();
-    	        
-    	        
-//     	        $query = $db->getQuery(true);
-//                 // =========================================================================================
-//     	        // Create Mapping Record - need newly created teamdid, programid, regid (set divid to 0)
-//     	        // =========================================================================================
-//     	        $columns = array(
-//     	            'id',
-//     	            'programid',
-//     	            'teamid',
-//     	            'divisionid',
-//     	            'regid',
-//     	            'published'
-//     	        );
-//     	        $values = array(
-//     	            0,
-//     	            $db->quote($item->programid), // programid (from registration record)
-//     	            $db->quote($new_row_id), // team id - determined by previous insert into team table
-//     	            $db->quote(0), // divisionid - set to zero (0) as divisional assignment has not been made yet
-//     	            $db->quote($item->id), // Registration ID
-//     	            0 // published - SET TO ZERO (0)
-//     	        );
-    	        
-//     	        $query->insert($db->quoteName('#__jsports_map'))
-//     	        ->columns($db->quoteName($columns))
-//     	        ->values(implode(',', $values));
-//     	        $db->setQuery($query);
-//     	        $db->execute();
-    	        
+// 	        $query = $db->getQuery(true);
 	        
-//     	        // =========================================================================================
-//     	        //  Set registration record to published.
-//     	        // =========================================================================================
-//     	        $query = $db->getQuery(true);
-//     	        $fields = array($db->quoteName('published') . ' = 1');
-//     	        $conditions = array($db->quoteName('id') . ' = ' .$item->id);
-    	        
-//     	        $query->update($db->quoteName('#__jsports_registrations'))
-//     	        ->set($fields)
-//     	        ->where($conditions);
-//     	        $db->setQuery($query);
-//     	        $db->execute();
-    	        
-//     	        // commit transaction
-//     	        $db->transactionCommit();
+// 	        $query->delete($db->quoteName('#__jsports_registrations'))
+// 	        ->whereIn($db->quoteName('id'), $pks);
+	        
+// 	        $db->setQuery($query);
+	        
+// 	        $db->execute();
 	        
 	        
-// 	        } catch (Exception $e) {
-// 	            // // catch any database errors.
-// 	            $db->transactionRollback();
-// 	            Factory::getApplication()->enqueueMessage("REgistration publish failed for one or more registration records", 'error');
-// 	            return false;
-	            
-// 	        }
-// 	        return true;
+	        
 // 	    }
-
-	    
-	    /**
-	     * Method to DELETE one or more records from the database
-	     *
-	     * @param   array    &$pks   A list of the primary keys to change.
-	     * @param   integer  $value  The value of the published state.
-	     *
-	     * @return  boolean  True on success.
-	     *
-	     * @since   4.0.0
-	     */
-	    public function delete(&$pks, $value = 1) {
-	        
-	        
-	        
-	        /* this is a very simple method to change the state of each item selected */
-	        $db = $this->getDatabase();
-	        
-	        $query = $db->getQuery(true);
-	        
-	        $query->delete($db->quoteName('#__jsports_registrations'))
-	        ->whereIn($db->quoteName('id'), $pks);
-	        $db->setQuery($query);
-	        $db->execute();
-	    }
 	    
 }
