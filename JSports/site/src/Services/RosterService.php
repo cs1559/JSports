38a78c12d47fb10005f63c04883b885bbe5d1c5c
cs155 <cs155@CS1559-LENOVA.lan>
2024-12-04 14:45:24 -0600

    Enhancements to support filtering substitutes.

--- JSports/site/src/Services/RosterService.php
+++ JSports/site/src/Services/RosterService.php
@@ -13,6 +13,7 @@
 namespace FP4P\Component\JSports\Site\Services;
 
 use FP4P\Component\JSports\Administrator\Table\RostersTable;
+use FP4P\Component\JSports\Site\Services\ProgramsService;
 use Joomla\Database\ParameterType;
 use Joomla\CMS\Factory;
 use FP4P\Component\JSports\Site\Objects\Application as Myapp;
@@ -79,31 +80,101 @@ class RosterService
     }
       
     
+    /**
+     * Function to retrieve STAFF members from the roster.  this function passes a
+     * hard coded value of 'S' to filter the roster list.
+     * 
+     * @param int $teamid
+     * @param int $programid
+     * @return array
+     */
     public static function getRosterStaff($teamid, $programid) {
         return RosterService::getRosterDataByType($teamid, $programid, 'S');
     }
 
-    public static function getRosterPlayers($teamid, $programid) {
-        return RosterService::getRosterDataByType($teamid, $programid, 'P');
+    /**
+     * This function retrieves a list of PLAYERS from the roster.  The function uses a 
+     * hard coded value of 'P' ot filter the roster list.  Additionally, one of the input 
+     * parameters is a boolean that indicates of the return array should include 
+     * substitute players on the roster.
+     * 
+     * @param int $teamid
+     * @param int $programid
+     * @param boolean $includesubs
+     * @return array
+     */
+    public static function getRosterPlayers($teamid, $programid, $includesubs = true) {
+        return RosterService::getRosterDataByType($teamid, $programid, 'P', $includesubs);
     }
     
     
-    private static function getRosterDataByType($teamid, $programid, $classification) {
+    /**
+     * This is a private function used to retrieve roster items filtered based on the input
+     * parameters.
+     * 
+     * @param int $teamid
+     * @param int $programid
+     * @param String $classification
+     * @param boolean $includesubs
+     * @return unknown
+     */
+    private static function getRosterDataByType($teamid, $programid, $classification, $includesubs = true) {
         $db = Factory::getDbo();
         $query = $db->getQuery(true);
-        
+              
         $query->select('p.*');
         $query->from($db->quoteName('#__jsports_rosters') . ' AS p ');
-        $conditions = array(
-            $db->quoteName('p.classification') . ' = ' . $db->quote($classification),
-            $db->quoteName('p.teamid') . ' = ' . $db->quote($teamid),
-            $db->quoteName('p.programid') . ' = ' . $db->quote($programid)
-        );
+        
+        /* If the results should filter out substitute players */
+        if ($includesubs) {      
+            $conditions = array(
+                $db->quoteName('p.classification') . ' = ' . $db->quote($classification),
+                $db->quoteName('p.teamid') . ' = ' . $db->quote($teamid),
+                $db->quoteName('p.programid') . ' = ' . $db->quote($programid)
+            );
+        } else {
+            $conditions = array(
+                $db->quoteName('p.classification') . ' = ' . $db->quote($classification),
+                $db->quoteName('p.teamid') . ' = ' . $db->quote($teamid),
+                $db->quoteName('p.programid') . ' = ' . $db->quote($programid),
+                $db->quoteName('p.substitute') . ' = 0'
+            );
+        }
         $query->where($conditions);
         $db->setQuery($query);
         return $db->loadObjectList();
         
     }
     
+    public static function getPlayerCount($teamid, $programid, $includesubs = true) {
+        return count(RosterService::getRosterPlayers($teamid, $programid, $includesubs));
+    }
+    
+    /**
+     * This is a roster function that checks to see if the program/season requires
+     * roster limits and compares current size to max roster size and returns a boolean.
+     * 
+     * @param unknown $teamid
+     * @param unknown $programid
+     * @return boolean
+     */
+    public static function canAddPlayers($teamid, $programid) {
+        $program = ProgramsService::getItem($programid);
+        
+        $currentplayers = RosterService::getPlayerCount($teamid, $programid, $program->includesubstitutes);
+        
+        $limitroster = $program->limitroster;
+        $rostersize = $program->rostersize;
+        
+        if (!$program->limitroster) {
+            return true;
+        }
+        
+        if ($rostersize <= $currentplayers) {
+            return false;
+        }
+        
+        return true;
+    }
     
 }
