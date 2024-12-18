cc59b127c93d68f8a78948f6b35ac2e6e4b40a01
Chris Strieter <cs1559@sbcglobal.net>
2024-12-17 19:08:44 -0600

    Tools view.

---
+++ JSports/admin/src/View/Tools/HtmlView.php
@@ -0,0 +1,112 @@
+<?php
+/**
+ * JSports - Joomla Sports Management Component
+ *
+ * @version     1.0.0
+ * @package     Tools.Administrator
+ * @subpackage  com_jsports
+ * @copyright   Copyright (C) 2023-2024 Chris Strieter
+ * @license     GNU/GPLv2, see http://www.gnu.org/licenses/gpl-2.0.html
+ *
+ */
+namespace FP4P\Component\JSports\Administrator\View\Tools;
+
+defined('_JEXEC') or die;
+
+use Joomla\CMS\Factory;
+use Joomla\CMS\Helper\ContentHelper;
+use Joomla\CMS\Language\Text;
+use Joomla\CMS\MVC\View\GenericDataException;
+use Joomla\CMS\MVC\View\HtmlView as BaseHtmlView;
+use Joomla\CMS\Toolbar\Toolbar;
+use Joomla\CMS\Toolbar\ToolbarHelper;
+
+/**
+ * View to edit an article.
+ *
+ * @since  1.6
+ */
+class HtmlView extends BaseHtmlView
+{
+    /**
+     * The \JForm object
+     *
+     * @var  \JForm
+     */
+    protected $form;
+    
+    /**
+     * The active item
+     *
+     * @var  object
+     */
+    protected $item;
+    
+    /**
+     * The model state
+     *
+     * @var  object
+     */
+    protected $state;
+    
+    /**
+     * The actions the user is authorised to perform
+     *
+     * @var  \JObject
+     */
+    protected $canDo;
+    
+    /**
+     * Execute and display a template script.
+     *
+     * @param   string  $tpl  The name of the template file to parse; automatically searches through the template paths.
+     *
+     * @return  mixed  A string if successful, otherwise an Error object.
+     *
+     * @throws \Exception
+     * @since   1.6
+     */
+    public function display($tpl = null)
+    {
+        $this->items         = $this->get('Items');
+        $this->pagination    = $this->get('Pagination');
+        $this->state         = $this->get('State');
+        $this->filterForm    = $this->get('FilterForm');
+        $this->activeFilters = $this->get('ActiveFilters');
+        
+        $this->addToolbar();
+        
+        return parent::display($tpl);
+    }
+    
+    /**
+     * Add the page title and toolbar.
+     *
+     * @return  void
+     *
+     * @throws \Exception
+     * @since   1.6
+     */
+    protected function addToolbar()
+    {
+        // Get the toolbar object instance
+        $toolbar = Toolbar::getInstance('toolbar');
+        
+//         ToolbarHelper::title(Text::_('Sports Managment - Programs'));
+
+        ToolbarHelper::title(
+            Text::_('COM_JSPORTS_TOOLS_PAGE_TITLE')
+            );
+               
+        $canDo = ContentHelper::getActions('com_jsports');
+                            
+        $toolbar->standardButton('dashboard')
+        ->icon('fa fa-home')
+        ->text('Dashboard')
+        ->task('display.dashboard')
+        ->listCheck(false);
+        $toolbar->preferences('com_jsports');
+        ToolbarHelper::help('help.html', true);       
+        
+    }
+}
