8d3f80e13a766d86c9f4a5da76e2089d1e840697
Chris Strieter <cs1559@sbcglobal.net>
2025-01-24 04:57:08 -0600

    deprecated admin serivce - migrating to site service

--- JSports/site/src/Services/LeagueService.php
+++ JSports/site/src/Services/LeagueService.php
@@ -7,10 +7,10 @@
  * @subpackage  com_jsports
  * @copyright   Copyright (C) 2023-2024 Chris Strieter
  * @license     GNU/GPLv2, see http://www.gnu.org/licenses/gpl-2.0.html
- * @deprecated
+ *
  */
 
-namespace FP4P\Component\JSports\Administrator\Services\LeagueService;
+namespace FP4P\Component\JSports\Site\Services\LeagueService;
 
 /**
  *
