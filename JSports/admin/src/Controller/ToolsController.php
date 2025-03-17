d0d5c058ff4c6db9d369ae64a7f051ad6d817273
Chris Strieter <cs1559@sbcglobal.net>
2025-03-17 06:00:10 -0500

    Added new tool to manually refresh standings

--- JSports/admin/src/Controller/ToolsController.php
+++ JSports/admin/src/Controller/ToolsController.php
@@ -19,7 +19,9 @@ use Joomla\CMS\Component\ComponentHelper;
 use Joomla\CMS\Factory;
 use FP4P\Component\JSports\Site\Services\LogService;
 use FP4P\Component\JSports\Site\Objects\Adapters\NSProAdapter;
-
+use FP4P\Component\JSports\Site\Objects\Standings\StandingsEngine;
+use FP4P\Component\JSports\Site\Services\ProgramsService;
+// /use FP4P\Component\JSports\Site\Services\LogService;
 
 
 class ToolsController extends BaseController
@@ -73,5 +75,55 @@ class ToolsController extends BaseController
         $this->setRedirect('index.php?option=com_jsports&view=tools');
         
     }
-    
+ 
+    public function refreshStandings($cachable = false, $urlparams = array()) {
+        
+        $params = ComponentHelper::getParams('com_jsports');   
+        $salt = $params->get('cronkey');
+        
+        $app = Factory::getApplication();
+        
+        $output = new \stdclass();
+        $output->datetime = date('l jS \of F Y h:i:s A');
+        $output->content = "";
+        
+//         $input = Factory::getApplication()->input;
+        $site = 'https://swibl.org';
+        $content = '';
+        
+        $cron_key = md5('JSports Key For: ' . $site . $salt);
+        
+        ob_start();
+        echo $output->datetime . "<br/>\n";
+        print "Update Standings:  START <br/>\n";
+        
+        $engine = new StandingsEngine();
+        
+        // 2024-03-11 Made changes to support issue#8 - remove hardcoded value.
+        // Retrieve the non completed programs.  'true' will filter only ACTIVE programs.
+        $programs = ProgramsService::getNonCompletedPrograms(true);
+        foreach ($programs as $program) {
+            print "- Processing Program " . $program->id . "<br/>\n";
+            $engine->generateStandings($program->id);
+        }
+        print "Update Standings:  END <br/>\n";
+        $msize = ob_get_length();
+//         header("Content-Length: $msize");
+//         header('Connection: close');
+        //$content = ob_get_contents();
+        $content = ob_get_clean();
+        
+        //print $content;
+        //$app->close();
+        
+        $msg = 'Standings were manually refreshed';
+        Factory::getApplication()->enqueueMessage($msg, 'message');
+        LogService::info("MANUAL REFRESH STANDINGS: <br/>\n" . $content);
+        
+        $this->setRedirect('index.php?option=com_jsports&view=tools');
+        
+        
+        
+        
+    }
 }
--- JSports/admin/tmpl/tools/default.php
+++ JSports/admin/tmpl/tools/default.php
@@ -29,6 +29,7 @@ These are a collection of tools that can be used to manage your JSports installa
     <table>
         <tr><td><a href="index.php?option=com_jsports&task=tools.newsletterImport"><button class="btn btn-primary">Newsletter Import</button></a></td><td>Import Contact Emails into Newsletter Program</td></tr>
         <tr><td><a href="index.php?option=com_jsports&task=tools.purgeLogs"><button class="btn btn-primary">Purge Log records</button></a></td><td>Purge recoreds from the JSports log table.</td></tr>
+        <tr><td><a href="index.php?option=com_jsports&task=tools.refreshStandings"><button class="btn btn-primary">Refresh Standings</button></a></td><td>Purge recoreds from the JSports log table.</td></tr>
     </table>
 
 <form action="<?php echo Route::_('index.php?option=com_jsports&view=tools'); ?>" method="post" name="adminForm" id="adminForm">
