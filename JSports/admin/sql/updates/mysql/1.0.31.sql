b23d81922ed68acaa50153a2a41e4754e2b26dcf
Chris Strieter <cs1559@sbcglobal.net>
2025-11-23 18:53:37 -0600

    updated sql to modify team profile table

---
+++ JSports/admin/sql/updates/mysql/1.0.31.sql
@@ -0,0 +1,3 @@
+alter table #__jsports_team add tournament tinyint default 0;
+alter table #__jsports_team add show_contact_info tinyint default 0;
+alter table #__jsports_team add open_roster tinyint default 0;
