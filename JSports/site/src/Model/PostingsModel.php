24a435fb49a59f51058a223d483fdd12d8471299
Chris Strieter <cs1559@sbcglobal.net>
2026-01-20 05:07:36 -0600

    initial code to support postings view

---
+++ JSports/site/src/Model/PostingsModel.php
@@ -0,0 +1,152 @@
+<?php
+/**
+ * JSports - Joomla Sports Management Component
+ *
+ * @version     1.0.0
+ * @package     JSports.Site
+ * @subpackage  com_jsports
+ * @copyright   Copyright (C) 2023-2026 Chris Strieter
+ * @license     GNU/GPLv2, see http://www.gnu.org/licenses/gpl-2.0.html
+ *
+ */
+
+namespace FP4P\Component\JSports\Site\Model;
+
+defined('_JEXEC') or die;
+
+use Joomla\CMS\MVC\Model\ListModel;
+use Joomla\Database\ParameterType;
+use Joomla\CMS\Factory;
+use FP4P\Component\JSports\Site\Services\GameService;
+use FP4P\Component\JSports\Site\Services\ProgramsService;
+
+/**
+ * Methods supporting a list of Teams.
+ *
+ */
+class PostingsModel extends ListModel
+{
+    
+    /**
+     * @deprecated
+     */
+    protected $programid;
+    
+	/**
+	 * Constructor.
+	 *
+	 * @param   array  $config  An optional associative array of configuration settings.
+	 *
+	 * @since   1.6
+	 * @see     \Joomla\CMS\MVC\Controller\BaseController
+	 */
+	public function __construct($config = array())
+	{
+		if (empty($config['filter_fields']))
+		{   
+			$config['filter_fields'] = array(
+				'bulletintype', 'a.bulletintype',
+			);
+		}
+		parent::__construct($config);
+	}
+
+	/**
+	 * Method to auto-populate the model state.
+	 *
+	 * Note. Calling getState in this method will result in recursion.
+	 *
+	 * @param   string  $ordering   An optional ordering field.
+	 * @param   string  $direction  An optional direction (asc|desc).
+	 *
+	 * @return  void
+	 *
+	 * @since   1.6
+	 */
+	protected function populateState($ordering = 'a.name', $direction = 'asc')
+	{
+	        
+		$search = $this->getUserStateFromRequest($this->context . '.filter.search', 'filter_search');
+		$this->setState('filter.search', $search);
+
+		$bulletintype = $this->getUserStateFromRequest($this->context . '.filter.bulletintype', 'filter_bulletintype', '');
+		$this->setState('filter.bulletintype', $bulletintype);
+				
+		// List state information.
+		parent::populateState($ordering, $direction);
+	}
+
+	/**
+	 * Method to get a store id based on model configuration state.
+	 *
+	 * This is necessary because the model is used by the component and
+	 * different modules that might need different sets of data or different
+	 * ordering requirements.
+	 *
+	 * @param   string  $id  A prefix for the store id.
+	 *
+	 * @return  string  A store id.
+	 *
+	 * @since   1.6
+	 */
+	protected function getStoreId($id = '')
+	{
+		// Compile the store id.
+		$id .= ':' . $this->getState('filter.bulletintype');
+		return parent::getStoreId($id);
+	}
+
+	/**
+	 * Build an SQL query to load the list data.  NOTE:  This function/query should not load anything PER DESIGN
+	 *
+	 * @return  \Joomla\Database\DatabaseQuery
+	 *
+	 * @since   1.6
+	 */
+	
+	protected function getListQuery()
+	{
+	    
+	    // Create a new query object.
+	    $db    = $this->getDatabase();
+	    $query = $db->getQuery(true);
+	    
+	    // Select the required fields from the table.
+	    $query->select(
+	        $this->getState(
+	            'list.select',
+	            'a.*'
+	            )
+	        );
+	    
+	    $bulletintype          = (string) $this->getState('filter.bulletintype');
+	    $search                = (string) $this->getState('filter.search');
+	   	
+	   	$query->from($db->quoteName('#__jsports_bulletins') . ' AS a ');
+	   	$query->where($db->quoteName('a.approved') . ' = 1');
+
+	    // Filter by bulletin type`   
+	    if (!empty($bulletintype))
+	    {
+	        $query->where($db->quoteName('a.bulletintype') . ' = :bulletintype');
+	        $query->bind(':bulletintype', $bulletintype, ParameterType::INTEGER);
+	    }
+
+	    if (!empty($search))
+	    {
+	        $search = '%' . trim($search) . '%';
+	        $query->where('UPPER(concat(a.title,a.content)) LIKE upper(:search)')
+	           ->bind(':search', $search, ParameterType::STRING);
+	    }
+	        
+// 	    // Add the list ordering clause.
+// 	    $orderCol  = $this->state->get('list.ordering', 'a.name');
+// 	    $orderDirn = $this->state->get('list.direction', 'asc');
+	    
+	    $query->order('a.createdate desc');
+	    
+	    return $query;
+	    
+	}	    
+
+}
---
+++ JSports/site/src/View/Postings/HtmlView.php
@@ -0,0 +1,114 @@
+<?php
+/**
+ * JSports - Joomla Sports Management Component
+ *
+ * @version     1.0.0
+ * @package     JSports.Site
+ * @subpackage  com_jsports
+ * @copyright   Copyright (C) 2023-2026 Chris Strieter
+ * @license     GNU/GPLv2, see http://www.gnu.org/licenses/gpl-2.0.html
+ *
+ */
+
+namespace FP4P\Component\JSports\Site\View\Postings;
+
+defined('_JEXEC') or die;
+
+use Joomla\CMS\MVC\View\HtmlView as BaseHtmlView;
+use FP4P\Component\JSports\Site\Services\ProgramsService;
+use FP4P\Component\JSports\Site\Services\SecurityService;
+use Joomla\CMS\MVC\View\GenericDataException;
+use Joomla\CMS\Pagination\Pagination;
+use Joomla\CMS\Form\Form;
+use FP4P\Component\JSports\Site\Model\TeamsModel;
+/**
+ * This is the TEAMS view that allows a user to list teams.
+ * @author Chris Strieter
+ *
+ */
+class HtmlView extends BaseHtmlView
+{
+    public $form;
+    
+    /**
+     * An array of items
+     *
+     * @var  array
+     */
+    protected $items;
+    
+    /**
+     * The pagination object
+     *
+     * @var  Pagination
+     */
+    protected $pagination;
+    
+    /**
+     * The model state
+     *
+     * @var  object
+     */
+    protected $state;
+    
+    /**
+     * Form object for search filters
+     *
+     * @var  Form
+     */
+    public $filterForm;
+    
+    /**
+     * The active search filters
+     *
+     * @var  array
+     */
+    public $activeFilters;
+    
+    public $isProgramPending;
+    
+    public function display($tpl = null)
+    {
+        
+        /** @var TeamsModel $model */  
+        $model = $this->getModel();
+        
+        $this->items         = $model->getItems();
+        $this->pagination    = $model->getPagination();
+        $this->state         = $model->getState();
+        $this->filterForm    = $model->getFilterForm();
+        $this->activeFilters = $model->getActiveFilters();
+        
+        $this->isProgramPending = false;
+        
+        /* removed 12/6/2024 - this caused pagination issues */
+        //$this->pagination->limit = 30;
+        
+        $programid = (int) ($this->state->get('filter.programid') ?? 0);
+        $this->program = ProgramsService::getItem($programid);
+        
+        $isAdmin = SecurityService::isAdmin();
+        $status = $this->program->status ?? '';
+        $setupFinal = !empty($this->program->setupfinal);
+        
+        $this->isProgramPending = ($status === 'P') || (!$setupFinal && (int) ($this->program->id ?? 0) > 0);
+        
+        if (!$this->program) {
+            $this->setLayout('noprogram');
+        }
+        
+        if ($this->state->get('filter.viewtype') === "C") {
+            $this->setLayout('completedgames');
+        }
+        // Check for errors.       
+        if (count($errors = $model->getErrors()))
+        {
+            throw new GenericDataException(implode("\n", $errors), 500);
+        }
+        
+        return parent::display($tpl);
+        
+    }
+    
+}
+
