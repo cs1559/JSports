18b58274c5cf9d669f4d46c17e1f87e2c27abdfd
Chris Strieter <cs1559@sbcglobal.net>
2026-02-09 05:33:54 -0600

    changed bulletin type table name

--- JSports/admin/sql/updates/mysql/1.2.1.sql
+++ JSports/admin/sql/updates/mysql/1.2.1.sql
@@ -1,12 +1,13 @@
-CREATE TABLE IF NOT EXISTS #__jsports_bulletin_types (
+CREATE TABLE IF NOT EXISTS #__jsports_bulletin_categories (
 	 category varchar(1) NOT NULL,
 	 catdesc varchar(30) not null,
 	 publicaccess tinyint default 0, 
  PRIMARY KEY (`category`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
-insert into #__jsports_bulletin_types values('G','General',0);
-insert into #__jsports_bulletin_types values('T','Tournament',0);
-insert into #__jsports_bulletin_types values('Y','Tryout',1);
-insert into #__jsports_bulletin_types values('F','Fundraising',0);
+insert into #__jsports_bulletin_categories values('G','General',0);
+insert into #__jsports_bulletin_categories values('T','Tournament',0);
+insert into #__jsports_bulletin_categories values('Y','Tryout',1);
+insert into #__jsports_bulletin_categories values('F','Fundraising',0);
+insert into #__jsports_bulletin_categories values('S','Sponsors',0);
 
