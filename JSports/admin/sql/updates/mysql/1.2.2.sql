8bd9312cb54ef777aa391cffd8ab7fae2ae13606
Chris Strieter <cs1559@sbcglobal.net>
2026-03-14 06:39:06 -0500

    removed the "IF NOT EXIST" from the add column statement

--- JSports/admin/sql/updates/mysql/1.2.2.sql
+++ JSports/admin/sql/updates/mysql/1.2.2.sql
@@ -1,3 +1,3 @@
 -- Version 1.2.2 - introduce archived team attribute for FUTURE USE
 ALTER TABLE `#__jsports_teams` 
-    ADD COLUMN IF NOT EXISTS `archived` TINYINT(1) NOT NULL DEFAULT 0 AFTER `properties`;
+    ADD COLUMN `archived` TINYINT(1) NOT NULL DEFAULT 0 AFTER `properties`;
