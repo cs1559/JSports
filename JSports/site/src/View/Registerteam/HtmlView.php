a36d1368bbf7f550c650f93704870826875d249d
Chris Strieter <cs1559@sbcglobal.net>
2026-07-13 14:20:33 -0500

    process of being deprecated

--- JSports/site/src/View/Registerteam/HtmlView.php
+++ JSports/site/src/View/Registerteam/HtmlView.php
@@ -5,10 +5,10 @@
  * @version     1.0.0
  * @package     JSports.Site
  * @subpackage  com_jsports
- * @copyright   Copyright (C) 2023-2024 Chris Strieter
+ * @copyright   Copyright (C) 2023-2026 Chris Strieter
  * @license     GNU/GPLv2, see http://www.gnu.org/licenses/gpl-2.0.html
  *
- * NOTE:  CURRENTLY NOT USED
+ * This
  */
 
 namespace FP4P\Component\JSports\Site\View\Registerteam;
